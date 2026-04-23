801bc103652a306be279c8c00e8a3b95959e0c95
Manuel Lorenzo <manulorenzop@gmail.com>
2026-04-23 10:25:49 +0200

    feat: configure application beans, scheduling, and create dashboard UI

--- src/main/java/me/manulorenzo/github_activity_stream/GithubActivityStreamApplication.java
+++ src/main/java/me/manulorenzo/github_activity_stream/GithubActivityStreamApplication.java
@@ -2,12 +2,12 @@ package me.manulorenzo.github_activity_stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GithubActivityStreamApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GithubActivityStreamApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActivityStreamApplication.class, args);
+    }
 }

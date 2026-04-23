d558c3667db4594e282653449571babf5eb6d493
Manuel Lorenzo <manulorenzop@gmail.com>
2026-04-23 10:27:29 +0200

    add RestTemplateConfig

--- src/main/java/me/manulorenzo/github_activity_stream/config/RestTemplateConfig.java
+++ src/main/java/me/manulorenzo/github_activity_stream/config/RestTemplateConfig.java
@@ -1,4 +1,13 @@
 package me.manulorenzo.github_activity_stream.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class RestTemplateConfig {
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
